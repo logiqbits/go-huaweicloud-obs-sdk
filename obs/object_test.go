a5c513cf1f0d55070235434296464b15e08bcddc
Rafiul Islam <avoidcloud@gmail.com>
2023-01-15 15:55:34 +0600

    removed secret keys

--- obs/object_test.go
+++ obs/object_test.go
@@ -9,11 +9,11 @@ import (
 )
 
 const (
-	endpoint     = "https://obs.ap-southeast-3.myhuaweicloud.com"
-	accessKey    = "JBH8RXIAAE9X9HBWKANB"
-	secretKey    = "8W1hSERaKD6aUTiqXEq96N7woKLXRD78c41bJeeH"
-	bucket       = "logiqbits-public-bucket"
-	customDomain = "https://static-cdn.shopap.io"
+	endpoint     = "https://obs.region.myhuaweicloud.com"
+	accessKey    = ""
+	secretKey    = ""
+	bucket       = ""
+	customDomain = ""
 )
 
 func TestSimpleFileUpload(t *testing.T) {
